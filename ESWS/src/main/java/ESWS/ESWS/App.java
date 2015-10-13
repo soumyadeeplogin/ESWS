ab5088a31589bcb706fd8ed08330487e6a76df0c
soumyadeeplogin <soumyadeeplogin@gmail.com>
2015-10-13 12:12:03 +0530

    Fresh Setup of Maven Project with no dependency

---
+++ ESWS/src/main/java/ESWS/ESWS/App.java
@@ -0,0 +1,13 @@
+package ESWS.ESWS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
